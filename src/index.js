f5f6b3657f0ad8a3c894e37ee65e80a0c8ae0f6c
PhanDat2004 <dat65531@gmail.com>
2025-06-11 23:02:35 +0700

    feat: restructure project and add handlebars support
    - Updated main entry point to src/index.js
    - Added express-handlebars for templating
    - Created basic server setup with routes for home and news pages
    - Implemented SCSS for styling with variables
    - Added Bootstrap for responsive design
    - Created layout and partials for header and footer
    - Added nodemon configuration for easier development
    - Included logo image and basic styles

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("combined"));
+
+const hbs = exphbs.create({
+  extname: ".hbs",
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "resources", "views", "layouts"),
+  partialsDir: path.join(__dirname, "resources", "views", "partials"),
+});
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
