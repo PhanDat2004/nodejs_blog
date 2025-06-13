2342d0ba6f2f42f9b1bcaababa5d9ae660713f80
PhanDat2004 <dat65531@gmail.com>
2025-06-13 18:55:39 +0700

    feat: update package.json and package-lock.json to add husky and lint-staged for pre-commit hooks; format code in controllers and index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const path = require("path");
-const exphbs = require("express-handlebars");
-const morgan = require("morgan");
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
-const route = require("./routes");
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
 
 const hbs = exphbs.create({
-  extname: ".hbs",
-  defaultLayout: "main",
-  layoutsDir: path.join(__dirname, "resources", "views", "layouts"),
-  partialsDir: path.join(__dirname, "resources", "views", "partials"),
+    extname: '.hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'resources', 'views', 'partials'),
 });
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources", "views"));
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
