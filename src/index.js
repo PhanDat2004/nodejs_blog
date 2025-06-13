fba83edd466b650061b49bb1a14cf1bfaf271001
PhanDat2004 <dat65531@gmail.com>
2025-06-13 19:43:05 +0700

    test prettier format

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
 
 const hbs = exphbs.create({
-    extname: '.hbs',
-    defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
-    partialsDir: path.join(__dirname, 'resources', 'views', 'partials'),
+  extname: '.hbs',
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'resources', 'views', 'partials'),
 });
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
@@ -22,5 +22,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
