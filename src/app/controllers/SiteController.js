da2000b8e5be654521af8aa03b58eedd67b0fea6
PhanDat2004 <dat65531@gmail.com>
2025-06-14 12:26:44 +0700

    feat: add mongoose and Course model, update database connection

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,20 @@
-class SiteController {
-  // [GET] /
-  index(req, res) {
-    res.render('home');
-  }
+const Course = require('../models/Course');
 
-  // [GET] / search
-  search(req, res) {
-    res.render('search');
-  }
+class SiteController {
+    // [GET] /
+    async index(req, res) {
+        try {
+            const courses = await Course.find({});
+            res.send(courses);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+
+    // [GET] / search
+    search(req, res) {
+        res.render('search');
+    }
 }
 
 module.exports = new SiteController();
