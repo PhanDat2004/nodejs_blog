e84668ba0b2b8fc02541f7f585cb4d39429e384e
PhanDat2004 <dat65531@gmail.com>
2025-06-14 15:47:24 +0700

    style: standardize code formatting and improve readability across multiple files feat: enhance home page layout and display course details fix: correct header branding text

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,16 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    async index(req, res) {
-        try {
-            const courses = await Course.find({});
-            res.send(courses);
-        } catch (err) {
-            res.status(500).send(err);
-        }
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     // [GET] / search
