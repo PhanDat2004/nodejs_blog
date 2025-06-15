e16c323f94457bff6f1ce3267c83cd543f0fd7c1
PhanDat2004 <dat65531@gmail.com>
2025-06-15 07:43:18 +0700

    refactor: remove mongoose utility functions and simplify CourseController and SiteController

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,13 +1,13 @@
 const Course = require('../models/Course');
-const { mongooseToObject } = require('../../util/mongoose');
 
 class CourseController {
     // [GET] /course/:slug
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
+            .lean()
             .then((course) =>
                 res.render('courses/show', {
-                    course: mongooseToObject(course),
+                    course,
                 }),
             )
             .catch(next);
--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,13 @@
 const Course = require('../models/Course');
-const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
     index(req, res, next) {
         Course.find({})
+            .lean()
             .then((courses) => {
                 res.render('home', {
-                    courses: mutipleMongooseToObject(courses),
+                    courses,
                 });
             })
             .catch(next);
